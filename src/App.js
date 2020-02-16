8997283402ece8f7b76bfc0a90f19101f208370a
Abraham Sanchez <sanchezbaj@gmail.com>
2020-02-16 16:42:29 -0400

    Commit for push to remote repository

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
@@ -10,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="primary">
+          Hola Mundo!
+        </Button>
       </header>
     </div>
   );
